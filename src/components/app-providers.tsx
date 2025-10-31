fbdf758da5e4f70b44c6f7754634b1e1206652bb
0xdorifto <gastaosousafaria@gmail.com>
2025-10-31 00:19:15 +0000

    fix: theme

--- src/components/app-providers.tsx
+++ src/components/app-providers.tsx
@@ -8,7 +8,7 @@ import React from 'react'
 export function AppProviders({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <ReactQueryProvider>
-      <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+      <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
         <SolanaProvider>{children}</SolanaProvider>
       </ThemeProvider>
     </ReactQueryProvider>
